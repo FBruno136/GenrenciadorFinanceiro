c647baaeddaf6fe9266f8e7785e39842256dd052
Bruno <bruno.fernandes136@gmail.com>
2021-11-02 18:51:20 -0300

    Cadastro e login experimental telas em andamento

--- app/src/main/java/com/example/genrenciadorfinanceiro/activitys/Aplicacao/PrincipalActivity.java
+++ app/src/main/java/com/example/genrenciadorfinanceiro/activitys/Aplicacao/PrincipalActivity.java
@@ -1,12 +1,10 @@
 package com.example.genrenciadorfinanceiro.activitys.Aplicacao;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
 import com.example.genrenciadorfinanceiro.R;
 import com.example.genrenciadorfinanceiro.databinding.ActivityPrincipalBinding;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -19,42 +17,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityPrincipalBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //indicando o contesto da classe
+        setContentView(R.layout.activity_principal);
 
-        binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarPrincipal.toolbar);
-
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.principal, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
+
